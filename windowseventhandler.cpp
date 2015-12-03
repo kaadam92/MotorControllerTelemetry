1abebb0ab54e4ba04f5fb888e2663bdbb08da5de
David Kiss <kiss.david92@gmail.com>
2015-12-03 16:17:13 +0100

    Kikommentetztem egy függvényt, hogy forduljon

--- windowseventhandler.cpp
+++ windowseventhandler.cpp
@@ -27,3 +27,18 @@ void WindowsEventHandler::stopCommand()
 {
     qDebug() << "Stop gomb megnyomva.";
 }
+/*
+void MainWindowsEventHandler::historyChanged()
+{
+    // Ahhoz, hogy frissüljenek a QML oldali adatok, frissíteni kell a változók összekapcsolását.
+    qmlContext.setContextProperty(QStringLiteral("historyModel"), QVariant::fromValue(history.stateList));
+    qmlContext.setContextProperty(QStringLiteral("currentState"), QVariant::fromValue(history.currentState));
+
+    qmlContext.setContextProperty(QStringLiteral("historyGraphTimestamps"), QVariant::fromValue(history.graphTimestamps));
+    qmlContext.setContextProperty(QStringLiteral("historyGraphVelocity"), QVariant::fromValue(history.graphVelocities));
+    qmlContext.setContextProperty(QStringLiteral("historyGraphAcceleration"), QVariant::fromValue(history.graphAcceleration));
+
+    // Jelzünk a QML controloknak, hogy újrarajzolhatják magukat, beállítottuk az új értékeket.
+    emit historyContextUpdated();
+}
+*/
