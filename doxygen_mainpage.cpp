b8bc4f60dc9b58e01fd72170ef16f7a71ff3c4ac
David Kiss <kiss.david92@gmail.com>
2015-12-10 22:27:00 +0100

    Alakul,alakul

--- doxygen_mainpage.cpp
+++ doxygen_mainpage.cpp
@@ -10,18 +10,32 @@
  *        - Az autóhoz a gombok segítségével lehet csatlkozni, illvet üzemkész állapotbe helyezni azt. A gombok alatt található slider
  *            segítségével sebesség alapjelet adhatunk a szimulázornak.
  *        - Az alkalmazás fő eleme a felül, jobb oldalt található megjelenítő rész. Itt három nézetet közül lehet váalsztani:
+ *        
  *            -- A pillanatnyi paramétereket lehet megjeleníteni
  *            -- Grafikonon követhetjük a paraméterek alakulásást. (A sebesség alapjelét és a valós sebességét)
  *            -- A 12 cellás akkumlátor egyes cellefeszültségeinek alakulását követhetjük nyomon
+ *            
  *        - Az alsó harmadban külnböző log üzeneteket olvashatunk.
  *
  *
  *
- * @section A program használata
+ * @section Usage A program használata
  *
  * Első lépésben mind a Simulator, mind a Telmetria szoftvert indítsuk el. Ezt követően a telemetria szoftverben a "Connenct" gomb seítségével
  * csatlakozhatunk a szimulátorhoz. Csatlakozás után a "Teszt" gomb segítségével elindíthatjuk az öntesztelő funkciót, melynek sikerességéről log üzenetek tájékoztatnak minket.
  * A "HV Enable" gomb segítségével engedélyezhetjük a nagyfeszültségű akkumlátor kimenetét. A sikeres állapotváltásta gomb zöldé válás jelzi. A "Drive Enable" gomb lenyomásával
- * engedélyezzük az uató hajtását. Ezen állapotban a slider segítségével adhatunk sebesség alapjelet.
+ * engedélyezzük az ató hajtását. Ezen állapotban a slider segítségével adhatunk sebesség alapjelet.
+ * 
+ * @section buildup Az alkalmazás felépítése
+ * 
+ * Az alkalmazás felépítése az alábbi ábraán látható. A viszonylag kompakt méret tartása érdekében csak a saját osztályainkat ábárzoltuk, a Qt
+ * osztályait nem.
+ * 
+ * ![](UML_main.png)
+ * 
+ * Az osztályok alapvetően három csoportra bonthatóak:
+ * 		- Kommunikációval kapcsoaltos osztályok
+ * 		- Adatfeldolgozással kapcsolatos osztályok
+ * 		- Megjelenítéssel, grafikus felület menedzselésével kapcsolatos osztályok.
  *
  */
