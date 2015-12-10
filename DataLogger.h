960ee61ad97a489c14e97cc43784a2b38a25d496
koincidencia <spamkazy@gmail.com>
2015-12-10 22:30:28 +0100

    Komm-ment.

--- DataLogger.h
+++ DataLogger.h
@@ -31,16 +31,35 @@ class DataLogger : public QObject
 public:
     /** @brief Konstruktor.
      *
-     *
+     * @param logFilePath   A log fájl elérési útja.
+     * @param logFileStylePath  A stílust leíró fájl elérési útja.
+     * @param strLogFilePath    A szöveges üzeneteket tároló fájl elérési útja.
      */
-    DataLogger(const QString&, const QString&, const QString&);
+    DataLogger(const QString& logFilePath, const QString& logFileStylePath, const QString& strLogFilePath);
+    /** @brief Destruktor.
+     *
+     * Elmenti a fájlokat, nehogy lezáratlanul maradjanak egy program elszállás esetén.
+     */
     ~DataLogger();
 private:
+    /** @brief Fájlba írás timer.
+     *
+     * Minden timeoutkor lekérjük az adatokat és elmentjük a megfelelő fájlba.
+     * @warning Ha túl gyakran írunk, az a program nagyfokú lassulását okozza!
+     */
     QTimer loggerTimer;
     QFile logFile, logFileStyle, strLogFile;
+
+    /** @brief A kiírás sorrendjét tartalmazó vector.
+     *
+     * A logFileStyle fájlból kiolvasott fejléc alapján elmentjük a fejléc stringjeit.
+     * Ezek a karakterláncok fognak a megkapott adat tároló kulcsaként szolgálni. @see DataParser::dataMap
+     **/
     QVector<QString> printOrder;
 
+    /** @brief A log fájlokba beleírja a fejlécet. Initkor hívódik meg.*/
     void printLogFileHeader();
+
 signals:
     void errorOccurred(const QString&);
     void getDataToLog();
